918efbad469a4ce3d1165c10d60c33f6756ccb31
raavisaibhargav5-del <raavisaibhargav5@gmail.com>
2026-05-05 23:45:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  // Replace 'My-Portfolio' with your exact GitHub repository name
   base: '/My-Portfolio/', 
-});
+})
