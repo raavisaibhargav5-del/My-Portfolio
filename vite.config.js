4ce065f7c29921dc459e0cb632155f7b500e5100
raavisaibhargav5-del <raavisaibhargav5@gmail.com>
2026-05-05 23:40:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  // Add this line below to match your GitHub repository name
   base: '/My-Portfolio/', 
 });
