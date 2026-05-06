7d3b0510c3fa494389db8832011575001d999271
raavisaibhargav5-del <raavisaibhargav5@gmail.com>
2026-05-05 23:28:18 -0400

    Add files via upload

---
+++ src/sections/About.jsx
@@ -0,0 +1,68 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Passionate about enterprise security
+    I build resilient, high-performance defenses
+    from detection to remediation`;
+
+  const aboutText = `Obsessed with securing enterprise environments—from proactive threat hunting to bulletproof incident response. Every log analyzed is a promise: security that organizations can trust.
+  When I’m not securing networks:
+⚡️ Building out SIEM and SOAR detection labs
+🔍 Diving deep into MITRE ATT&CK frameworks
+🛡️ Automating security workflows with Python and Bash
+☁️ Exploring IAM and cloud security operations`;
+
+  const imgRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+    
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Defend with purpose, Built to secure"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man.jpg"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
